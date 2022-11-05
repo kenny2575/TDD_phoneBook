9a77fe2e96b4e94be048a09da8abdcc1a6b06383
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2022-11-05 19:11:32 +0700

    stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -26,4 +26,20 @@ public class PhoneBookTest {
         assertThrows(expected, () -> sut.add(null, null));
     }
 
+    @Test
+    void addNonUniqueRecord(){
+        int expected = 1;
+        sut.add("Test Name", "8-800-355-55-23");
+        assertEquals(expected,  sut.add("Test Name", "8-800-355-55-00"));
+    }
+
+    @Test
+    void findByNumberTest(){
+        String expected = "Test Name";
+        sut.add("Test Name", "8-800-355-55-23");
+        sut.add("Test Surname", "8-800-355-55-00");
+        sut.add("Test", "8-800-355-55-44");
+        assertEquals(expected, sut.findByNumber("8-800-355-55-23"));
+    }
+
 }
