8632932e9eebc083cc17355772a352ed67761623
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2022-11-05 18:55:12 +0700

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,10 @@
+import java.util.TreeMap;
+
 public class PhoneBook {
+
+    TreeMap<String, String> phoneBook = new TreeMap<>();
+
+    public int add(String name, String number){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+
+    PhoneBook sut;
+
+    @BeforeEach
+    void initial(){
+        sut = new PhoneBook();
+    }
+
+    @Test
+    void addReturnTest(){
+       int expected = 1;
+
+       assertEquals(expected,  sut.add("Test Name", "8-800-355-55-00"));
+
+    }
+
+    @Test
+    void addCheckTest(){
+        var expected = NullPointerException.class;
+        assertThrows(expected, () -> sut.add(null, null));
+    }
+
 }
